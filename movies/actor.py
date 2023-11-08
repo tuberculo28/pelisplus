c0ffdd74dd0aac455efb0b058a9766dfbab5118b
Brandon <tuberculosis1287@gmail.com>
2023-11-08 12:06:40 -0300

    s

--- movies/actor.py
+++ movies/actor.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, jsonify, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
 
@@ -7,6 +7,7 @@ from werkzeug.exceptions import abort
 from movies.db import get_db
 
 bp = Blueprint('actor', __name__, url_prefix="/actor/")
+bpapi = Blueprint('api_actor', __name__, url_prefix="/api/actor/")
 
 @bp.route('/')
 def index():
@@ -15,3 +16,11 @@ def index():
         'SELECT * FROM actor ORDER BY last_name, first_name ASC'
     ).fetchall()
     return render_template('actor/index.html', actors=actors)
+
+@bpapi.route('/')
+def api_index():
+    db = get_db()
+    actors = db.execute(
+        'SELECT * FROM actor ORDER BY last_name, first_name ASC'
+    ).fetchall()
+    return jsonify(actors=actors)
